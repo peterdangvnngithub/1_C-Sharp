f99a7a54230efd8c792b7bf39bc84487bc9d2b52
Đặng Quốc Hưng <takako.hungdq@gmail.com>
2022-06-22 10:52:29 +0700

    Add project files.

---
+++ 1_C-Sharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
